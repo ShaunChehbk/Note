c40824447f964721609821ce3721d6fd9fe1b0fc
y30035646 <654247481@qq.com>
2023-02-24 22:33:25 +0800

    Add block status

--- Analyzer.js
+++ Analyzer.js
@@ -11,11 +11,42 @@ class Analyzer {
         if (c == '$') {
             //if next == $ then 'block mode'
             console.log('Normal -> Inline');
-            this.process = this.atInline;
+            this.process = this.atMath;
             this.result.push(this.current);
             this.current = "";
         } else if (c == '\0') {
             this.result.push(this.current);
+        } else {
+            this.current += c;
+        }
+    }
+
+    atMath(c) {
+        //相邻的$$视为block标识符, $_$为空inline
+        if (c == '$') {
+            console.log('Normal -> Block')
+            this.process = this.atBlock
+        } else if (c == '\0') {
+            //Error
+        } else {
+            //atInline
+            this.process = this.atInline
+        }
+    }
+
+    outBlock(c) {
+        if (c == '$') {
+            this.process = this.atNormal;
+            this.result.push(this.current);
+            this.current = "";
+        } else {
+            //Error
+        }
+    }
+
+    atBlock(c) {
+        if (c == '$') {
+            this.process = this.outBlock;
         } else {
             this.current += c;
         }
@@ -36,9 +67,9 @@ class Analyzer {
     }
 }
 
-string = "This is normal $ This is inline math $, and here we go normal again$\0"
+string = "This is normal $ This is inline math $, and here we go normal again$$$Block$$\0"
 
-let a = new Analyzer("This is normal $ This is inline math $, and here we go normal again$");
+let a = new Analyzer("This is normal $ This is inline math $, and here we go normal again$$$Block$$");
 for (var i = 0; i < string.length; i++) {
     a.process(string.charAt(i));
 }
