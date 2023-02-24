281bfd37638e6ae3bcea7b4c4346f495605b056d
y30035646 <654247481@qq.com>
2023-02-24 22:01:28 +0800

    Fix 'cannot read the endof the string'

--- Analyzer.js
+++ Analyzer.js
@@ -1,6 +1,8 @@
 
 class Analyzer {
-    constructor() {
+    constructor(string) {
+        this.string = string + '\0';
+        console.log(this.string);
         this.process = this.atNormal
         this.result = []
         this.current = ""
@@ -12,6 +14,8 @@ class Analyzer {
             this.process = this.atInline;
             this.result.push(this.current);
             this.current = "";
+        } else if (c == '\0') {
+            this.result.push(this.current);
         } else {
             this.current += c;
         }
@@ -23,6 +27,8 @@ class Analyzer {
             this.process = this.atNormal;
             this.result.push(this.current);
             this.current = "";
+        } else if (c == '\0') {
+            console.log("Error");
         } else {
             this.current += c;
         }
@@ -30,9 +36,9 @@ class Analyzer {
     }
 }
 
-string = "This is normal $ This is inline math $, and here we go normal again"
+string = "This is normal $ This is inline math $, and here we go normal again$\0"
 
-let a = new Analyzer();
+let a = new Analyzer("This is normal $ This is inline math $, and here we go normal again$");
 for (var i = 0; i < string.length; i++) {
     a.process(string.charAt(i));
 }
